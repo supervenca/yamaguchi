e0537ba5ff9ac842b32ac1434d317ffb06208c03
Ventseslava Shopova <supervenca@yandex.ru>
2022-04-30 17:08:06 +0300

    button functions in js

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ window.addEventListener('DOMContentLoaded', () => {
     const upButton = document.getElementById('up');
     const downButton = document.getElementById('down');
 
+    const tableTop = document.getElementById('table-top');
+    const tableBottom = document.getElementById('table-bottom');
+
     function upTable() {
         console.log('up');
+        tableBottom.style.marginTop = '10%';
+        tableTop.style.marginTop = '-10%';
     }
     
     function downTable() {
         console.log('down');
+        tableBottom.style.marginTop = '';
+        tableTop.style.marginTop = '';
     }
 
     upButton.addEventListener('click', upTable);
